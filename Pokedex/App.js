7c8b1908ae81477a6e3d436b0fcbb7b6f42c7875
Ian Andrewson <ianandrewson@gmail.com>
2019-11-01 01:07:58 -0700

    implemented PokemonList with static props; ready for API input or SearchSort

--- Pokedex/App.js
+++ Pokedex/App.js
@@ -2,9 +2,12 @@ import Component from '../Component.js';
 import Header from './Header.js';
 import SearchSort from './SearchSort.js';
 import PokemonList from './PokemonList.js';
+import staticPokemon from '../test/staticPokemon.js';
 
 class App extends Component{
     onRender(){
+        let props = { pokemon: staticPokemon };
+
         const header = new Header();
         const headerDOM = header.renderDOM();
         document.body.appendChild(headerDOM);
@@ -12,6 +15,10 @@ class App extends Component{
         const searchSort = new SearchSort();
         const searchSortDOM = searchSort.renderDOM();
         document.body.appendChild(searchSortDOM);
+
+        const pokemonList = new PokemonList(props);
+        const pokemonListDOM = pokemonList.renderDOM();
+        document.body.appendChild(pokemonListDOM);
     }
 
     renderHTML(){
--- Pokedex/PokemonList.js
+++ Pokedex/PokemonList.js
@@ -1,13 +1,21 @@
 import Component from '../Component.js';
 import PokemonItem from './PokemonItem.js';
+import pokemon from '../test/staticPokemon.js';
 
 class PokemonList extends Component {
-    onRender(){
-
+    onRender(el){
+        let listItemsArray = this.props.pokemon;
+        
+        listItemsArray.forEach(element => {
+            const props = { pokemon : element };
+            const item = new PokemonItem(props);
+            const itemDOM = item.renderDOM();
+            el.appendChild(itemDOM);
+        });
     }
 
     renderHTML(){
-
+        return /*html*/`<section class="pokedex"></section>`;
     }
 }
 
